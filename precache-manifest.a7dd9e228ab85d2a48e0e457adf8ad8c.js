3802e67a81191a541d9cd8f63e99f7729926c129
Ali Raza Sherazi <syedd.sherazi@gmail.com>
2019-02-06 22:21:08 +0500

    Updates

---
+++ precache-manifest.a7dd9e228ab85d2a48e0e457adf8ad8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "765ed4c2c517f8ab1f3e",
+    "url": "/jet-setter-react/static/js/runtime~main.765ed4c2.js"
+  },
+  {
+    "revision": "1a225b74c96e881143f1",
+    "url": "/jet-setter-react/static/js/main.1a225b74.chunk.js"
+  },
+  {
+    "revision": "96182a5a7c230b207e33",
+    "url": "/jet-setter-react/static/js/1.96182a5a.chunk.js"
+  },
+  {
+    "revision": "1a225b74c96e881143f1",
+    "url": "/jet-setter-react/static/css/main.d80b1aa0.chunk.css"
+  },
+  {
+    "revision": "73aaf0e6e22e009bb52ae3eff2bf62b0",
+    "url": "/jet-setter-react/index.html"
+  }
+];
